4c2ee290df1a35f3033f96806cfd161f25602e47
Sushobhan Pal <sushobhanpalofficial@gmail.com>
2026-03-24 08:21:47 +0530

    Delete practice03 directory

--- practice03/1.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-a = np.array([10,20,30,40])
-print(a)
--- practice03/8.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-arr = np.array([73,1,2,34,27])
-print(np.sort(arr)) #arr.sort() returns None
-
-print(np.sort(arr)[::-1]) #argsort() returns indexes of the sorted array
